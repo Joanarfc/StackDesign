b2f7f45b71e47d5987267ae00e297193cbb1d05a
Joanarfc <joanarfcarvalho@hotmail.com>
2022-06-26 21:28:49 +0100

    Added the logic to the application

---
+++ StackDesign/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            int number1 = 4;
+            int number2 = 10;
+            string text = "Have a wonderful day!";
+
+            Console.WriteLine("Result of the Push:");
+
+            //test the Pop of an empty stack:
+            //stack.Pop();
+
+            stack.Push(number1);
+            stack.Push(number2);
+            stack.Push(text);
+
+
+            //test the Push of a null object into the stack:
+            //stack.Push(null);
+
+
+            //test the Pop in an empty stack:
+            //stack.Clear();
+            Console.WriteLine("\nResult of the Pop:");
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackDesign/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackDesign
+{
+    public class Stack
+    {
+        private readonly List<object> _listOfObjects = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("It's not possible to add a null object to the Stack.");
+
+            _listOfObjects.Add(obj);
+            Console.WriteLine(obj);
+        }
+
+        public object Pop()
+        {
+            if (_listOfObjects == null || _listOfObjects.Count == 0)
+                throw new InvalidOperationException("Pop is not possible since there are no elements in the Stack.");
+
+            int index = _listOfObjects.Count - 1;
+            object returnObj = _listOfObjects[index];
+            _listOfObjects.RemoveAt(index);
+            return returnObj;
+        }
+
+        public void Clear()
+        {
+            _listOfObjects.Clear();
+        }
+    }
+}
